6dba907e587fc0f0b524a1a3e30ef59f33e293a6
Mulk Abdulhadi <mabdulul@gmail.com>
2020-07-11 11:02:48 -0400

    nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -20,20 +20,20 @@ const Nav = (props) => {
 			>
 				<span className="navbar-toggler-icon"></span>
 			</button>
-			<div className="collapse navbar-collapse" id="navbarNav">
-				<ul className="navbar-nav">
-					<li className="nav-item active">
-						<a className="nav-link" href="#">
-							Home <span className="sr-only">(current)</span>
+			<div className="collapse navbar-collapse navSet" id="navbarNav">
+				<ul className="navbar-nav navSetDrill">
+					<li className="nav-item ">
+						<a className="nav-link nav-border" href="#">
+							Home
 						</a>
 					</li>
 					<li className="nav-item">
-						<a className="nav-link" href="#">
+						<a className="nav-link nav-border" href="#">
 							Legendary
 						</a>
 					</li>
 					<li className="nav-item">
-						<a className="nav-link" href="#">
+						<a className="nav-link nav-border" href="#">
 							Phase
 						</a>
 					</li>
