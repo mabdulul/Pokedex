03f8b209844d87744c0b8f8a4964f4a3508f1ed2
Mulk Abdulhadi <mabdulul@gmail.com>
2020-10-13 13:42:06 -0400

    yaya

--- src/components/GetPokemon.jsx
+++ src/components/GetPokemon.jsx
@@ -1,7 +1,9 @@
 import history from "./history";
 
 export const getPokemon = async (url) => {
-	const response = await fetch(url);
+	const response = await fetch(
+		`https://cors-anywhere.herokuapp.com/https://pokeapi.glitch.me/v1/pokemon/${url}`
+	);
 	const data = await response.json();
 	const thedata = history.push("/list", { data });
 	return thedata;
--- src/components/utils.jsx
+++ src/components/utils.jsx
@@ -1,6 +1,7 @@
 export const loadData = async (url) => {
-	const response = await fetch(url);
-
+	const response = await fetch(
+		`https://cors-anywhere.herokuapp.com/https://pokeapi.glitch.me/v1/pokemon/${url}`
+	);
 	const data = await response.json();
 	return data;
 };
